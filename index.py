a138b5d5f4b5113ad0f64dea9db1f1fd0d2cf687
kmbn <github@nevermindtheumlauts.com>
2019-04-17 21:29:57 +0200

    Make nicer URLs

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from filter_feeds import FEEDS, filter_feed
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        feed_name = parse_qs(self.path[2:])["q"][0]
+        feed_name = parse_qs(self.path.split("?")[1])["feed"][0]
         filtered_feed = filter_feed(*FEEDS.get(feed_name))
         self.send_response(200)
         self.send_header("Content-type", "application/rss+xml")
