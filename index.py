b7e0a8d43459d18a1fe76bf9d4dd43dd9cd070e9
kmbn <github@nevermindtheumlauts.com>
2019-04-17 19:57:14 +0200

    Only expose index.py module…
    …and move logic to a separate module.

--- index.py
+++ index.py
@@ -1,51 +1,7 @@
 from http.server import BaseHTTPRequestHandler
-from typing import Callable
 from urllib.parse import parse_qs
 
-import requests
-
-from bs4 import BeautifulSoup as Soup
-
-FilterFunc = Callable[[Soup], bool]
-
-
-def newyorker(item: Soup) -> bool:
-    return not item.link.text.startswith("https://www.newyorker.com/magazine/")
-
-
-def nybooks(item: Soup) -> bool:
-    return not item.guid.text.startswith("https://www.nybooks.com/articles/")
-
-
-def guardian(item: Soup) -> bool:
-    url = item.link.text
-    sport = url.startswith("https://www.theguardian.com/sport/")
-    football = url.startswith("https://www.theguardian.com/football/")
-
-    return sport or football
-
-
-FEEDS = {
-    "newyorker": ("https://www.newyorker.com/feed/magazine/rss", newyorker),
-    "nybooks": ("https://feeds.feedburner.com/nybooks", nybooks),
-    "guardian": ("https://www.theguardian.com/international/rss", guardian),
-}
-
-
-def get_feed(url: str) -> str:
-    r = requests.get(url, timeout=5)
-    return r.text
-
-
-def filter_feed(url: str, filter_func: FilterFunc) -> str:
-    xml = get_feed(url)
-    soup = Soup(xml, "xml")
-
-    for item in soup.find_all("item"):
-        if filter_func(item):
-            item.decompose()
-
-    return str(soup)
+from filter_feeds import FEEDS, filter_feed
 
 
 class handler(BaseHTTPRequestHandler):
